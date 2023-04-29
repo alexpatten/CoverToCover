05c7844ae9a0df0ab5f5f2871cde8344c4d1c0c4
Alex Patten <103220430+alexpatten@users.noreply.github.com>
2023-04-29 13:00:26 -0400

    Updates

--- Cover to Cover/Cover_to_Cover.py
+++ Cover to Cover/Cover_to_Cover.py
@@ -2,7 +2,14 @@ import csv
 import random
 import urllib.request
 import io
-from PIL import Image
+from tkinter import *
+from tkinter.ttk import *
+from PIL import Image, ImageTk
+
+random_book = NONE
+show_info = False
+books = []
+myLikedGenres = []
 
 # Parse CSV and store all genres in a list
 genres = []
@@ -20,18 +27,32 @@ for i, genre in enumerate(genres):
     print(f"{i+1}. {genre}")
 print("Select a genre:")
 selection = int(input()) - 1
-myGenre = genres[selection]
+myGenre = genres[selection].replace("'", "").replace("[", "").replace("]", "")
 
-while True:
+def rand_book():
+    global random_book
     # Select a random book with the selected genre
-    books = []
     with open('C:\\Users\\alext\\Downloads\\books\\books.csv', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             if myGenre in row['genres']:
                 books.append(row)
     random_book = random.choice(books)
-    print(f"Title: {random_book['title']}")
+
+rand_book()
+
+def update_book():
+    rand_book()
+    # Select a random book with the selected genre
+    with open('C:\\Users\\alext\\Downloads\\books\\books.csv', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            if myGenre in row['genres']:
+                books.append(row)
+    random_book = random.choice(books)
+
+    #Update title label
+    title_lbl.config(text=f"Title: {random_book['title']}", font=("Helvetica", 20))
 
     # Open the cover image in the program
     img_url = random_book['coverImg']
@@ -40,34 +61,72 @@ while True:
 
     # Create a file-like object from the image data
     image_file = io.BytesIO(image_data)
+    cover = Image.open(image_file).resize((400, 600))
+    cov_img = ImageTk.PhotoImage(cover)
+    cov_lbl.configure(image=cov_img)
+    cov_lbl.image = cov_img
 
-    # Open the image using Pillow and display it
-    image = Image.open(image_file)
-    image.show()
-
-    # Prompt user for more information
-    print("Would you like to see more information?")
-    response = input().lower()
-    if response == 'yes':
-        image.close()
-        # Display additional data for the selected book
-        print(f"Author: {random_book['author']}")
-        print(f"Rating: {random_book['rating']}")
-        print(f"Description: {random_book['description']}")
-        print(f"Pages: {random_book['pages']}")
-        print(f"Liked Percent: {random_book['likedPercent']}")
-
-        # Store all genres from the selected book in a list
-        myLikedGenres = []
-        for genre in random_book['genres'].split(', '):
-            if genre not in myLikedGenres:
-                myLikedGenres.append(genre)
-
-        # Prompt user for the next book
-        print("Would you like to see the next random book?")
-        response = input().lower()
-        if response != 'yes':
-            image.close()
-            exit()
+def show_book_info():
+    global show_info
+    if show_info == False:
+        show_info = True
+        author_lbl.config(text=f"Author: {random_book['author']}", font=("Helvetica", 12))
+        rating_lbl.config(text=f"Rating: {random_book['rating']}", font=("Helvetica", 12))
+        description_lbl.config(text=f"Description: {random_book['description']}", font=("Helvetica", 12), anchor=CENTER)
+        pages_lbl.config(text=f"Pages: {random_book['pages']}", font=("Helvetica", 12))
     else:
-        image.close()
+        show_info = False
+        author_lbl.config(text="")
+        rating_lbl.config(text="")
+        description_lbl.config(text="")
+        pages_lbl.config(text="")
+        update_book()
+
+#Create UI
+root = Tk()
+root.title("Cover to Cover")
+
+# Set window attributes
+#root.attributes('-fullscreen', True)
+
+# Create labels and buttons
+title_lbl = Label(root, text=f"Title: {random_book['title']}", font=("Helvetica", 20))
+title_lbl.pack(pady=20)
+
+# Open the cover image in the program
+img_url = random_book['coverImg']
+with urllib.request.urlopen(img_url) as url:
+    image_data = url.read()
+
+# Create a file-like object from the image data
+image_file = io.BytesIO(image_data)
+
+cover = Image.open(image_file).resize((400, 600))
+cov_img = ImageTk.PhotoImage(cover)
+cov_lbl = Label(root, image=cov_img)
+cov_lbl.image = cov_img
+cov_lbl.pack()
+
+x_btn_img = ImageTk.PhotoImage(Image.open("C:\\Users\\alext\\source\\repos\\Cover to Cover\\Cover to Cover\\resources\\x_btn.png").resize((128, 128)))
+x_btn = Button(root, image=x_btn_img, command=update_book)
+x_btn.image = x_btn_img
+x_btn.place(x=550, y=350)
+
+heart_btn_img = ImageTk.PhotoImage(Image.open("C:\\Users\\alext\\source\\repos\\Cover to Cover\\Cover to Cover\\resources\\heart_btn.png").resize((128, 128)))
+heart_btn = Button(root, image=heart_btn_img, command=show_book_info)
+heart_btn.image = heart_btn_img
+heart_btn.place(x=1225, y=350)
+
+author_lbl = Label()
+author_lbl.pack(pady=10)
+rating_lbl = Label()
+rating_lbl.pack(pady=10)
+description_lbl = Label(wraplength=800)
+description_lbl.pack(pady=10)
+pages_lbl = Label()
+pages_lbl.pack(pady=10)
+
+# Set window size to fit all widgets
+root.geometry("%dx%d" % (root.winfo_reqwidth(), root.winfo_reqheight()))
+
+root.mainloop()
