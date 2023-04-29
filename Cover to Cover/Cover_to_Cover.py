7e9b7e2b23e9de8e83c182f60c8f7e565f0d89b0
Alex Patten <103220430+alexpatten@users.noreply.github.com>
2023-04-29 10:08:04 -0400

    Updates

--- Cover to Cover/Cover_to_Cover.py
+++ Cover to Cover/Cover_to_Cover.py
@@ -1,6 +1,8 @@
 import csv
 import random
 import urllib.request
+import io
+from PIL import Image
 
 # Parse CSV and store all genres in a list
 genres = []
@@ -12,11 +14,11 @@ with open('C:\\Users\\alext\\Downloads\\books\\books.csv', encoding='utf-8') as 
                 genres.append(genre)
 
 # Display list of genres and prompt user to select one
-print("Select a genre:")
 for i, genre in enumerate(genres):
     # Remove apostrophes and brackets from genre string
     genre = genre.replace("'", "").replace("[", "").replace("]", "")
     print(f"{i+1}. {genre}")
+print("Select a genre:")
 selection = int(input()) - 1
 myGenre = genres[selection]
 
@@ -34,13 +36,20 @@ while True:
     # Open the cover image in the program
     img_url = random_book['coverImg']
     with urllib.request.urlopen(img_url) as url:
-        image = url.read()
-    # do something with the image data here...
+        image_data = url.read()
+
+    # Create a file-like object from the image data
+    image_file = io.BytesIO(image_data)
+
+    # Open the image using Pillow and display it
+    image = Image.open(image_file)
+    image.show()
 
     # Prompt user for more information
     print("Would you like to see more information?")
     response = input().lower()
     if response == 'yes':
+        image.close()
         # Display additional data for the selected book
         print(f"Author: {random_book['author']}")
         print(f"Rating: {random_book['rating']}")
@@ -57,23 +66,8 @@ while True:
         # Prompt user for the next book
         print("Would you like to see the next random book?")
         response = input().lower()
-        if response == 'yes':
-            # Select a random book with genres in myLikedGenres
-            books = []
-            with open('C:\\Users\\alext\\Downloads\\books\\books.csv', encoding='utf-8') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    if myGenre in row['genres'] and any(g in row['genres'] for g in myLikedGenres):
-                        books.append(row)
-            random_book = random.choice(books)
-            print(f"Title: {random_book['title']}")
+        if response != 'yes':
+            image.close()
+            exit()
     else:
-        # Select a random book with genres in myGenre or myLikedGenres
-        books = []
-        with open('C:\\Users\\alext\\Downloads\\books\\books.csv', encoding='utf-8') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            for row in csv_reader:
-                if myGenre in row['genres'] or any(g in row['genres'] for g in myLikedGenres):
-                    books.append(row)
-        random_book = random.choice(books)
-        print(f"Title: {random_book['title']}")
+        image.close()
