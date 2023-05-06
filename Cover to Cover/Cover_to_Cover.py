b7efd91db6591dd36d2265906224fd690a61a81d
Alex Patten <103220430+alexpatten@users.noreply.github.com>
2023-05-05 20:39:23 -0400

    Updates

--- Cover to Cover/Cover_to_Cover.py
+++ Cover to Cover/Cover_to_Cover.py
@@ -19,40 +19,6 @@ def get_options_from_csv(csv_path):
             options.append(row['genre'])
     return options
 
-def get_book_data(selected_genre):
-    endpoint = 'https://www.googleapis.com/books/v1/volumes'
-    params = {'q': f'subject:{selected_genre}', 'startIndex': str(random.randint(1, 100)), 'maxResults': '1'}
-
-    response = requests.get(endpoint, params=params)
-
-    if response.status_code == 200:
-        try:
-            book_data = response.json()['items'][0]['volumeInfo']
-            title = book_data['title']
-            authors = book_data['authors']
-            publisher = book_data['publisher']
-            published_date = book_data['publishedDate']
-            description = book_data['description']
-            categories = book_data['categories']
-            image_url = book_data['imageLinks']['thumbnail'].replace('zoom=1', 'zoom=3')
-
-            return {
-                'Selected Genre': selected_genre,
-                'Title': title,
-                'Authors': ", ".join(authors),
-                'Publisher': publisher,
-                'Published Date': published_date,
-                'Genres': ", ".join(categories),
-                'Description': description,
-                'Image URL': image_url
-            }
-
-        except (KeyError, IndexError):
-            return {'Error': 'No books found'}
-
-    else:
-        return {'Error': 'Request failed'}
-
 options = get_options_from_csv(csv_path)
 
 root = Tk()
@@ -82,25 +48,80 @@ canvas.configure(yscrollcommand=scrollbar.set)
 scrollbar.pack(side="right", fill="y")
 canvas.pack(side="left", fill=BOTH, expand=True)
 
-def show_book_data(event):
-    widget = event.widget
-    selection = widget.curselection()
-    if selection:
-        selected_genre = widget.get(selection[0])
-        book_data = get_book_data(selected_genre)
+label_list = []
 
-        for i, (key, value) in enumerate(book_data.items()):
-            label = Label(root, text=f'{key}: {value}')
-            label.pack(fill=BOTH, padx=10, pady=5)
+def get_book_data(event, selected_genre):
+    # Clear the existing labels
+    for label in label_list:
+        label.destroy()
+
+    # Get the selected genre from the Listbox
+    selected_genre = event.widget.get(event.widget.curselection())
+
+    endpoint = 'https://www.googleapis.com/books/v1/volumes'
+    params = {'q': f'subject:{selected_genre.replace(" ", "+")}', 'startIndex': str(random.randint(1, 100)), 'maxResults': '1'}
+
+    response = requests.get(endpoint, params=params)
+
+    if response.status_code == 200:
+        try:
+            book_data = response.json()['items'][0]['volumeInfo']
+
+            # Create and populate the labels with the book data
+            title_label = Label(scrollable_frame, text=f'Title: {book_data["title"]}')
+            title_label.pack(pady=(10, 0), padx=10, anchor=W)
             
-        # Fetch and display image from URL
-        response = requests.get(book_data['Image URL'])
-        img = Image.open(BytesIO(response.content))
-        photo = ImageTk.PhotoImage(img)
-        img_label = Label(root, image=photo)
-        img_label.image = photo
-        img_label.pack(fill=BOTH, padx=10, pady=5)
+            if "authors" in book_data:
+                author_label = Label(scrollable_frame, text=f'Author: {book_data["authors"][0]}')
+                author_label.pack(pady=10, padx=10, anchor=W)
+                
+            if "categories" in book_data:
+                genre_label = Label(scrollable_frame, text=f'Genre: {book_data["categories"]}')
+                genre_label.pack(pady=10, padx=10, anchor=W)
 
-genre_listbox.bind('<Double-Button-1>', show_book_data)
+            if "description" in book_data:
+                desc_label = Label(scrollable_frame, text=f'Description {book_data["description"]}')
+                desc_label.pack(pady=10, padx=10, anchor=W)
+
+            cover_label = Label(scrollable_frame)
+
+            # Get the URL for the book cover image
+            if "imageLinks" in book_data and "thumbnail" in book_data["imageLinks"]:
+                cover_url = book_data['imageLinks']['thumbnail'].replace('zoom=1', 'zoom=3')
+
+                # Get the image data from the URL
+                response = requests.get(cover_url)
+                img_data = response.content
+
+                # Convert the image data to a Tkinter PhotoImage
+                img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+
+                # Add the image to the cover label
+                cover_label.img = img
+                cover_label.configure(image=img)
+
+                # Add the labels to the window
+                cover_label.pack(pady=10, padx=10, anchor=CENTER)
+
+            else:
+                cover_label.pack(pady=10, padx=10, anchor=CENTER)
+                cover_label.configure(text="No cover available")
+
+            label_list.extend([title_label, author_label, genre_label, desc_label, cover_label])
+
+        except (KeyError, IndexError):
+            error_label = Label(scrollable_frame, text="No books found")
+            error_label.pack(pady=10, padx=10, anchor=CENTER)
+            label_list.append(error_label)
+
+    else:
+        error_label = Label(scrollable_frame, text="Request failed")
+        error_label.pack(pady=20)
+        label_list.append(error_label) # Add the error label to the list of labels
+
+        root.mainloop()
+
+
+genre_listbox.bind('<Double-Button-1>', lambda event: get_book_data(event, genre_listbox.get(genre_listbox.curselection())))
 
 root.mainloop()
